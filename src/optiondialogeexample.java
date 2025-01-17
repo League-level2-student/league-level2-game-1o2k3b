20575e298b9b3d1ad45a2be0e82f7547acd7921d
student <student@localhost>
2025-01-17 08:25:35 -0800

    gnksdajfs

--- src/optiondialogeexample.java
+++ src/optiondialogeexample.java
@@ -4,9 +4,14 @@ public class optiondialogeexample {
 
 public static void main(String[] args) {
 	JFrame frame = new JFrame();
-	String[] options = new String[2];
-	options[0] = "Film";
-	options[1] = "Tech";
-	JOptionPane.showOptionDialog(frame.getContentPane(), "film or tech", "1", 0, JOptionPane.INFORMATION_MESSAGE, null, options, null);
+	String[][] options = new String[10][2];
+	String characters[] = {"Film", "Tech", "breakfast", "lunch"};
+	for(int i = 0; i<10; i++) {
+	for(int p = 0; p<2; p++) {
+	options[i][p] = characters[i*p];
+
+}
+	}
+	JOptionPane.showOptionDialog(frame.getContentPane(), "film or tech", "1", 0, JOptionPane.INFORMATION_MESSAGE, null, options[0], null);
 }
 }
